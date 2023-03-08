7bf6ad5891ff800ead840202f76898e34f8fdd0c
Snehashish Biswas <teamscenariox@gmail.com>
2023-03-08 18:06:50 +0530

    Initial

---
+++ words.py
@@ -0,0 +1,5 @@
+Dwords = {
+  "TTL":"TTC stands for \"time to copy\", was used for the first time by @CoderX on 08 March 2023 on time 17:30\n\nExample:\n > Your friend: Hey bro see my awesome code:\n`print(\"chala ja bsdk\")`\n > You: TTC\n > Your Friend: JNL code output padh...",
+  "CBSE":"CBSE stands for central board of secondary education.. but accourding to our knowledge ye ek aaisa gandu board hai jo baccho ki kand bina sabun pani ke maar leta hai ratta marna toh iske khoon mai hai\n\nExample: > Your friend: Bhai cbse board hai mera..\n > You: Aree bc zazzra piche mudh toh, and screams aree bhai teri toh fati padi hai..\n > You: Toh bsdk sil de tu hi 🥲'
+}
+    
