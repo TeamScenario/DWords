76af87c70601cdd74b7609ae47c01a27a2ac01ae
Snehashish Biswas <teamscenariox@gmail.com>
2023-03-08 18:21:17 +0530

    Update words.py

--- words.py
+++ words.py
@@ -1,5 +1,15 @@
 Dwords = {
-  "TTL":"TTC stands for \"time to copy\", was used for the first time by @CoderX on 08 March 2023 on time 17:30\n\nExample:\n > Your friend: Hey bro see my awesome code:\n`print(\"chala ja bsdk\")`\n > You: TTC\n > Your Friend: JNL code output padh...",
-  "CBSE":"CBSE stands for central board of secondary education.. but accourding to our knowledge ye ek aaisa gandu board hai jo baccho ki gand bina sabun pani ke maar leta hai ratta marna toh iske khoon mai hai!\n\nExample: > Your friend: Bhai cbse board hai mera..\n > You: Aree bc zazzra piche mudh toh, and screams aree bhai teri toh fati padi hai..\n > You: Toh bsdk sil de tu hi 🥲"
+  "TTL":"""TTC stands for \"time to copy\", was used for the first time by @CoderX on 08 March 2023 on time 17:30
+  
+  Example:
+    > Your friend: Hey bro see my awesome code:`print(\"chala ja bsdk\")`
+    > You: TTC
+    > Your Friend: JNL code output padh...""",
+
+  "CBSE":"""CBSE stands for central board of secondary education.. but accourding to our knowledge ye ek aaisa gandu board hai jo baccho ki gand bina sabun pani ke maar leta hai ratta marna toh iske khoon mai hai!\n
+  
+  Example: 
+      > Your friend: Bhai cbse board hai mera..
+      > You: Aree bc zazzra piche mudh toh, and screams aree bhai teri toh fati padi hai..
+      > You: Toh bsdk sil de tu hi 🥲"""
 }
-    
